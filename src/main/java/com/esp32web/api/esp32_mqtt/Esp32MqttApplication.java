711fa90daba3b912308500a48f5917d0d13fcb78
CollapsedBanana <siriuspires78@gmail.com>
2025-02-27 15:14:44 +0100

    still going

--- src/main/java/com/esp32web/api/esp32_mqtt/Esp32MqttApplication.java
+++ src/main/java/com/esp32web/api/esp32_mqtt/Esp32MqttApplication.java
@@ -2,12 +2,12 @@ package com.esp32web.api.esp32_mqtt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.esp32web.api.esp32_mqtt")  // 🔹 Force Spring à scanner tous les packages
 public class Esp32MqttApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Esp32MqttApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Esp32MqttApplication.class, args);
+    }
 }
