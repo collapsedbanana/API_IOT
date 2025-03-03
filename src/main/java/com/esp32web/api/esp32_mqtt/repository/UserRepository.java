5d710e09bb38942602bb202e503cc0dc8fd9bbaa
CollapsedBanana <siriuspires78@gmail.com>
2025-03-03 22:44:53 +0100

    failed method user

--- src/main/java/com/esp32web/api/esp32_mqtt/repository/UserRepository.java
+++ src/main/java/com/esp32web/api/esp32_mqtt/repository/UserRepository.java
@@ -9,5 +9,4 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-    List<Capteur> findByUser(User user);
 }
