08e556f458bbdabf0ca30415a7b10acff430df23
CollapsedBanana <siriuspires78@gmail.com>
2025-03-06 11:39:38 +0100

    new test

--- src/main/java/com/esp32web/api/esp32_mqtt/model/Capteur.java
+++ src/main/java/com/esp32web/api/esp32_mqtt/model/Capteur.java
@@ -27,6 +27,7 @@ public class Capteur {
     private User user;
 
     public Capteur() {
+        // Constructeur par défaut
     }
 
     // Constructeur complet qui initialise toutes les valeurs
@@ -40,49 +41,72 @@ public class Capteur {
         this.user = user;
     }
 
+    // Nouveau constructeur simplifié (4 paramètres)
+    public Capteur(float temperature, float humidity, int luminositeRaw, int humiditeSolRaw) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.luminositeRaw = luminositeRaw;
+        this.humiditeSolRaw = humiditeSolRaw;
+        this.timestamp = LocalDateTime.now(); // Initialise automatiquement le timestamp
+    }
+
     // Getters et Setters
     public Long getId() {
         return id;
     }
+
     public float getTemperature() {
         return temperature;
     }
+
     public void setTemperature(float temperature) {
         this.temperature = temperature;
     }
+
     public float getHumidity() {
         return humidity;
     }
+
     public void setHumidity(float humidity) {
         this.humidity = humidity;
     }
+
     public int getLuminositeRaw() {
         return luminositeRaw;
     }
+
     public void setLuminositeRaw(int luminositeRaw) {
         this.luminositeRaw = luminositeRaw;
     }
+
     public int getHumiditeSolRaw() {
         return humiditeSolRaw;
     }
+
     public void setHumiditeSolRaw(int humiditeSolRaw) {
         this.humiditeSolRaw = humiditeSolRaw;
     }
+
     public LocalDateTime getTimestamp() {
         return timestamp;
     }
+
     public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
     }
+
     public String getDeviceId() {
         return deviceId;
     }
+
     public void setDeviceId(String deviceId) {
         this.deviceId = deviceId;
     }
+
     public User getUser() {
         return user;
     }
+
     public void setUser(User user) {
         this.user = user;
     }
